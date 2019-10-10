af14c831da490513d12804edef0ad481a16b8398
Mohammed Daaniyaal <35423314+iamdaaniyaal@users.noreply.github.com>
2019-10-10 16:06:26 +0530

    Update jenkins.sh

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -1,4 +1,8 @@
+sudo yum update -y
 sudo systemctl stop firewalld && sudo systemctl disable firewalld
+sudo echo 'export jenkinsip='$(curl -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)'' >> ~/.bash_profile
+sudo echo 'export sonarqubeip='$(curl -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/description)'' >> ~/.bash_profile
+source ~/.bash_profile
 sudo yum install epel-release -y 
 sudo yum install java-1.8.0-openjdk  java-1.8.0-openjdk-devel  wget  unzip -y 
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
@@ -7,11 +11,11 @@ sudo yum install jenkins maven google-cloud-sdk kubectl -y
 sudo echo "jenkins  ALL=(ALL)   NOPASSWD:  ALL" >> /etc/sudoers
 sudo  wget -O  /opt/docker.sh  https://get.docker.com && sudo chmod 755 /opt/docker.sh 
 sudo sh  /opt/docker.sh   &&  sudo usermod -aG  docker jenkins
-sudo cat /gcpterraform/scripts/mydaemon.json  >> /etc/docker/daemon.json
-sudo  mv /usr/share/maven/conf/*  /mnt && sudo cat /gcpterraform/scripts/mvn_sonar_settings.xml > /usr/share/maven/conf/settings.xml
+sudo cat /tmp/mydaemon.json  >> /etc/docker/daemon.json
+sudo  mv /usr/share/maven/conf/*  /mnt && sudo cat /tmp/mvn_sonar_settings.xml > /usr/share/maven/conf/settings.xml
 sudo systemctl restart docker &&  sudo systemctl enable  docker
 sudo systemctl restart jenkins &&  sudo systemctl enable  jenkins
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword >> /root/jenkins_pass
 sudo wget -P /opt/  https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.3.0.1492-linux.zip
 sudo unzip /opt/sonar-scanner-cli-3.3.0.1492-linux.zip -d /opt  &&  sudo mv /opt/sonar-scanner-3.3.0.1492-linux  /opt/sonar-scanner
-sudo echo "sonar.host.url=http://$sonarqubeip:9000" >> /opt/sonar-scanner/conf/sonar-scanner.properties
+sudo echo "sonar.host.url=http://${sonarqubeip}" >> /opt/sonar-scanner/conf/sonar-scanner.properties
