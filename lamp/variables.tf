02f77ef4297fb4514102f86fe7fca833cd47fbac
Mohammed Daaniyaal <35423314+iamdaaniyaal@users.noreply.github.com>
2019-10-11 15:22:45 +0530

    Create variables.tf

---
+++ lamp/variables.tf
@@ -0,0 +1,8 @@
+variable "credentials" {}
+variable "region" {}
+variable "gcp_project" {}
+variable "lamp_stack_instance_name" {}
+variable "lamp_stack_machine_type" {}
+variable "lamp_stack_zone" {}
+variable "lamp_stack_vpc_name" {}
+variable "lamp_stack_subnet_name" {}
